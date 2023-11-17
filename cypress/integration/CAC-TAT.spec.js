d40144641d2f61dd8380127e15ae3617aa73da1f
arieldimelo <33663321+arieldimelo@users.noreply.github.com>
2023-11-17 18:34:48 -0300

    ok

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,11 @@
 /// <reference types="Cypress" />
 
+const { should } = require("chai")
+
 describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html'),
-        cy.title('Central de Atendimento ao Cliente TAT')
+        cy.visit('./src/index.html')
+
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
   })
